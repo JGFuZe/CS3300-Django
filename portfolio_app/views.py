7b22efab26ba236a02670e7f63c54641b44401ba
JGFuZe <minemania36@gmail.com>
2023-09-13 17:59:52 -0600

    changed url settings

--- portfolio_app/views.py
+++ portfolio_app/views.py
@@ -2,9 +2,15 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
-
 def home(request):
+    return render( request, 'portfolio_app/index.html')
+
+
+
+
+def oldHome(request):
     return HttpResponse('Home Page')
+
 
 def test1(request):
     return HttpResponse('test1')
