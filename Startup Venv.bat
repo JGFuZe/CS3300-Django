a8959730400e1b47fa28209d7a9015b2be8b79d9
JGFuZe <minemania36@gmail.com>
2023-10-16 17:00:20 -0600

    GE04

---
+++ Startup Venv.bat
@@ -0,0 +1,4 @@
+C:\Code\CS3300\Portfolio\testEnv\Scripts\activate.bat
+
+cd Code\CS3300\Portfolio
+python manage.py runserver
--- portfolio_app/admin.py
+++ portfolio_app/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import Student, Portfolio, Project
+
+admin.site.register(Student)
+admin.site.register(Portfolio)
+admin.site.register(Project)
